3089ca9b0e5c5f1ca6c5b1638ae05505f86ebdfd
ThiasTux <mathias.ciliberto@gmail.com>
2019-03-26 17:18:09 +0000

    Added plotting of fitness function.

--- data_labelling_main.py
+++ data_labelling_main.py
@@ -1,7 +1,7 @@
 from data_processing import labelling_data as lbld
 
 if __name__ == '__main__':
-    lbld.prepare_annotation("/home/mathias/Documents/Datasets/BeachVolleyball/08032019_150000/")
+    # lbld.prepare_annotation("/home/mathias/Documents/Datasets/BeachVolleyball/08032019_150000/")
     # lbld.create_labelling_project_files("/home/mathias/Documents/Datasets/BeachVolleyball/20022019_110000/raw/")
-    # lbld.create_merge_file("/home/mathias/Documents/Datasets/BeachVolleyball/20022019_110000/user1/")
-    # lbld.process_data_3dmodel("/home/mathias/Documents/Datasets/BeachVolleyball/20022019_110000/")
+    # lbld.create_merge_file("/home/mathias/Documents/Datasets/BeachVolleyball/20022019_110000/")
+    lbld.process_data_3dmodel("/home/mathias/Documents/Datasets/BeachVolleyball/08032019_150000/")
