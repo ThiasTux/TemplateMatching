Added selection of most similar template using LCS. Added SHL preview dataset. Fixed return of f1 score for isolated case. Fixed template generation: computation of fitness, mutation step, stream_labels parameter. Fixed template generation using encoding. Fixed best template selection after generation. Fixed dataset name in scores plotting.

data_processing/data_loader.py
@@ -1,10 +1,12 @@
 from data_processing.hcitable import HCITable
 from data_processing.opportunity import OpportunityDataset
+from data_processing.shl import SHLPreview
 from data_processing.skoda import Skoda
 from data_processing.skoda_mini import SkodaMini
 from data_processing.synthetic import Synthetic1, Synthetic2, Synthetic3, Synthetic4
 from data_processing.utd_mhad import UTDMhad
 from data_processing.hci import HCIGuided
+from utils import distance_measures as dtm
 
 import numpy as np
 
@@ -33,6 +35,8 @@ def load_training_dataset(dataset_choice='opportunity', template_choice_method='
         dataset = Synthetic3()
     elif dataset_choice == 'synthetic4':
         dataset = Synthetic4()
+    elif dataset_choice == 'shl_preview':
+        dataset = SHLPreview()
 
     streams, labels = dataset.load_isolated_dataset()
 
@@ -56,7 +60,19 @@ def load_training_dataset(dataset_choice='opportunity', template_choice_method='
         for c in classes:
             templates.append(streams[np.random.choice(np.where(labels == c)[0])])
     elif template_choice_method == 'mrt_lcs':
-        pass
+        for k, c in enumerate(classes):
+            tmp_templates = [streams[t] for t in np.where(labels == c)[0]]
+            matching_scores = np.zeros((len(tmp_templates), len(tmp_templates)), dtype=int)
+            for i in range(len(tmp_templates)):
+                for j in range(i + 1, len(tmp_templates)):
+                    d, c = dtm.LCS(tmp_templates[i], tmp_templates[j])
+                    matching_scores[i][j] = d
+                    matching_scores[j][i] = d
+            matching_scores_sums = np.sum(matching_scores, axis=0)
+            matching_scores_perc = np.array(
+                [matching_scores_sums[i] / len(tmp_templates[i]) for i in range(len(tmp_templates))])
+            ordered_indexes = np.argsort(matching_scores_perc)
+            templates.append(np.array(tmp_templates[ordered_indexes[-1]]))
     elif template_choice_method == 'mrt_dtw':
         pass
 

performance_evaluation/fitness_functions.py
@@ -63,7 +63,8 @@ def isolated_fitness_function_params(matching_scores, labels, thresholds, classe
         if precision != 0 and recall != 0:
             f1 = 2 / (1 / recall + 1 / precision)
         else:
-            return 0
+            f1 = 0
+        return f1
     elif parameter_to_optimize == 'f1_acc':
         tps = np.sum(true_positive)
         fps = np.sum(false_positive)

templates_generation_main.py
@@ -17,7 +17,7 @@ from training.templates.es_templates_generator import ESTemplateGenerator, ESTem
 from utils.plots import plot_creator as plt_creator
 
 if __name__ == '__main__':
-    dataset_choice = 'synthetic4'
+    dataset_choice = 'skoda'
     outputs_path = "/home/mathias/Documents/Academic/PhD/Research/WLCSSTraining/training/cuda"
 
     num_test = 1
@@ -28,24 +28,24 @@ if __name__ == '__main__':
     thresholds = list()
     null_class_percentage = 0.5
 
-    num_individuals = 256
-    rank = 256
+    num_individuals = 128
+    rank = 32
     elitism = 5
     iterations = 500
     fitness_function = 86
     crossover_probability = 0.3
     mutation_probability = 0.1
     inject_templates = False
-    optimize_thresholds = True
+    optimize_thresholds = False
 
     if dataset_choice == 'skoda':
         use_encoding = '3d'
-        classes = [3001, 3003, 3013, 3018]
-        # classes = [3001, 3002, 3003, 3005, 3013, 3014, 3018, 3019]
+        # classes = [3001, 3003, 3013, 3018]
+        classes = [3001, 3002, 3003, 3005, 3013, 3014, 3018, 3019]
         output_folder = "{}/skoda/params".format(outputs_path)
         null_class_percentage = 0.6
-        params = [55, 6, 0]
-        thresholds = [186, 104, 195, 115]
+        params = [57, 2, 8]
+        thresholds = [370, 353, 220, 233, 307, 463, 228, 135]
         bit_values = 15
     elif dataset_choice == 'skoda_mini':
         use_encoding = False
@@ -141,6 +141,11 @@ if __name__ == '__main__':
         params = [55, 54, 0]
         thresholds = [918, 929, 842, 883]
         bit_values = 8
+    elif dataset_choice == 'shl_preview':
+        use_encoding = False
+        classes = [1, 2, 4, 7, 8]
+        output_folder = "{}/shl_preview/params".format(outputs_path)
+        null_class_percentage = 0.5
 
     if inject_templates:
         templates, streams, streams_labels = dl.load_training_dataset(dataset_choice=dataset_choice,
@@ -191,11 +196,11 @@ if __name__ == '__main__':
             chromosomes = len(templates[i])
         else:
             chromosomes = int(np.ceil(
-                np.average([len(streams[i]) for i, sl in enumerate(streams_labels) if sl == c]).astype(int)) / 5)
+                np.average([len(streams[i]) for i, sl in enumerate(streams_labels) if sl == c]).astype(int)))
         print("{} - {}".format(c, chromosomes))
         if optimize_thresholds:
             optimizer = ESTemplateThresholdsGenerator(streams, tmp_labels, params, c, chromosomes, bit_values,
-                                                      chosen_template=templates[i],
+                                                      chosen_template=templates[i], use_encoding=use_encoding,
                                                       num_individuals=num_individuals, rank=rank,
                                                       elitism=elitism,
                                                       iterations=iterations,
@@ -204,7 +209,7 @@ if __name__ == '__main__':
                                                       mt_p=mutation_probability)
         else:
             optimizer = ESTemplateGenerator(streams, tmp_labels, params, thresholds[i], c, chromosomes, bit_values,
-                                            chosen_template=templates[i],
+                                            chosen_template=templates[i], use_encoding=use_encoding,
                                             num_individuals=num_individuals, rank=rank,
                                             elitism=elitism,
                                             iterations=iterations,
@@ -238,7 +243,6 @@ if __name__ == '__main__':
                 for k, item in enumerate(results[-1]):
                     f.write("{}\n".format(" ".join([str(x) for x in item.tolist()])))
 
-    best_templates = [np.stack((np.arange(len(r)), r), axis=1) for r in best_templates]
     output_file_path = join(output_folder,
                             "{}_templates_{}.txt".format(hostname, st))
     elapsed_time = time.time() - start_time
@@ -276,6 +280,20 @@ if __name__ == '__main__':
     fitness_score = ftf.isolated_fitness_function_params(mss, streams_labels, thresholds, classes,
                                                          parameter_to_optimize='f1')
     print(fitness_score)
+
+    for i, c in enumerate(classes):
+        tmp_labels = np.copy(streams_labels)
+        tmp_labels[tmp_labels != c] = 0
+        templates_fitness_score = ftf.isolated_fitness_function_templates(mss[:, i], tmp_labels, thresholds[i],
+                                                                          parameter_to_optimize=86)
+        print("Class: {} - Score: {:4.3f} - Good dist: {:4.3f} - Bad dist: {:4.3f} - Thres: {}".format(c,
+                                                                                                       templates_fitness_score[
+                                                                                                           0],
+                                                                                                       templates_fitness_score[
+                                                                                                           1],
+                                                                                                       templates_fitness_score[
+                                                                                                           2],
+                                                                                                       thresholds[i]))
 
     plt_creator.plot_templates_scores(output_file_path.replace(".txt", ""))
     plt_creator.plot_isolated_mss(mss, thresholds, dataset_choice, classes, streams_labels,

training/templates/es_templates_generator.py
@@ -18,7 +18,7 @@ class ESTemplateGenerator:
                  cr_p=0.3, mt_p=0.1,
                  elitism=3, rank=10, fitness_function=7, maximize=True):
         self.__streams = steams
-        self.__streams_labels = np.array(stream_labels).reshape((len(steams), 1))
+        self.__streams_labels = stream_labels
         self.__params = params
         self.__threshold = threshold
         self.__class = cls
@@ -119,9 +119,8 @@ class ESTemplateGenerator:
 
     def __compute_fitness_cuda(self, templates_pop):
         matching_scores = self.__m_wlcss_cuda.compute_wlcss(templates_pop)
-        matching_scores = [np.concatenate((ms, self.__streams_labels), axis=1) for ms in matching_scores]
         fitness_scores = np.array([fit_fun.isolated_fitness_function_templates(matching_scores[0][:, k],
-                                                                               matching_scores[0][:, -1],
+                                                                               self.__streams_labels,
                                                                                self.__threshold,
                                                                                parameter_to_optimize=self.__fitness_function)
                                    for k in
@@ -272,7 +271,7 @@ class ESTemplateThresholdsGenerator:
     def __mutation(self, templates_pop, thresholds_pop, mp):
         tmpl_sizes = templates_pop.shape
         mask = np.random.rand(templates_pop.shape[0], templates_pop.shape[1]) < mp
-        new_templates_pop_mask = np.random.normal(0, 8, size=tmpl_sizes) * mask
+        new_templates_pop_mask = np.random.normal(0, 2, size=tmpl_sizes) * mask
         new_templates_pop = np.remainder(np.copy(templates_pop) + new_templates_pop_mask, self.__bit_values)
 
         mask = np.random.rand(thresholds_pop.shape[0], thresholds_pop.shape[1]) < mp