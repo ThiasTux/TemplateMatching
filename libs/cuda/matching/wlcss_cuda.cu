1881abe3538be04b6e027680ba80f054b2994a24
ThiasTux <mathias.ciliberto@gmail.com>
2020-10-05 20:51:29 +0100

    Changed behaviour for CUDA libraries to exit when an error occurs.

--- libs/cuda/matching/wlcss_cuda.cu
+++ libs/cuda/matching/wlcss_cuda.cu
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=false)
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
 {
    if (code != cudaSuccess)
    {
--- libs/cuda/training/variable_templates/wlcss_cuda_variable_templates_training_2d_enc.cu
+++ libs/cuda/training/variable_templates/wlcss_cuda_variable_templates_training_2d_enc.cu
@@ -4,7 +4,7 @@
 #include "../distance.h"
 
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=false)
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
 {
    if (code != cudaSuccess)
    {
